017ed04224faa36694b3416291ff57f90bf36287
Nixon Cam <nixon.cam997@gmail.com>
2025-07-16 15:52:46 +1200

    Dress the clown completed woop woop

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -3,60 +3,48 @@
 // -    -   -   -   -  //
 
 document.addEventListener('DOMContentLoaded', () => {
-  let headIndex = 0
-  const headImg = document.getElementById('head')
+  
+  const partNames = ['head', 'body', 'shoes']
+  
 
-  function changeClownHead(direction = "forward") {
-    if (direction === "forward") {
-      headIndex = (headIndex + 1) % 6 
-    } else if (direction === "backward") {
-      headIndex = (headIndex - 1 + 6) % 6  
+  const images = {
+    head: document.getElementById('head'),
+    body: document.getElementById('body'),
+    shoes: document.getElementById('shoes')
+  }
+
+  const indexes = {
+    head: 0,
+    body: 0,
+    shoes: 0
+  }
+
+  let clothingIndex = 0
+
+  function changeClothing(direction = 'forward') {
+    const part = partNames[clothingIndex]
+    const max = 6
+
+    if (direction === 'forward') {
+      indexes[part] = (indexes[part] + 1) % max
+    } else {
+      indexes[part] = (indexes[part] - 1 + max) % max
     }
-    let headSrc = "./images/head" + (headIndex + 1) + ".png"
-    headImg.src = headSrc;
+
+    images[part].src = `./images/${part}${indexes[part] + 1}.png`
   }
-  
-  let bodyIndex = 0
-const bodyImg = document.getElementById('body')
 
-function changeClownBody(direction = "forward") {
-  if (direction === "forward") {
-    bodyIndex = (bodyIndex + 1) % 6
-  } else if (direction === "backward") {
-    bodyIndex = (bodyIndex - 1 + 6) % 6
-  }
-  let bodySrc = "./images/body" + (bodyIndex + 1) + ".png"
-  bodyImg.src = bodySrc
-}
-
-let shoesIndex = 0;
-const shoesImg = document.getElementById('shoes');
-
-function changeClownShoes(direction = "forward") {
-  if (direction === "forward") {
-    shoesIndex = (shoesIndex + 1) % 6
-  } else if (direction === "backward") {
-    shoesIndex = (shoesIndex - 1 + 6) % 6
-  }
-  let shoesSrc = "./images/shoes" + (shoesIndex + 1) + ".png";
-  shoesImg.src = shoesSrc
-}
-
-  headImg.addEventListener('click', () => changeClownHead("forward"))
-
-  
-document.addEventListener('keydown', (event) => {
-  if (event.key === "ArrowRight") {
-    changeClownHead("forward")
-  } else if (event.key === "ArrowLeft") {
-    changeClownHead("backward")
-  } else if (event.key === "ArrowUp") {
-    changeClownBody("forward")
-  } else if (event.key === "ArrowDown") {
-    changeClownBody("backward")
-  }
-})
-
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      changeClothing('forward')
+    } else if (event.key === 'ArrowLeft') {
+      changeClothing('backward')
+    } else if (event.key === 'ArrowUp') {
+      clothingIndex = (clothingIndex - 1 + 3) % 3
+    } else if (event.key === 'ArrowDown') {
+      clothingIndex = (clothingIndex + 1) % 3
+    }
+  })
 })
 
 
