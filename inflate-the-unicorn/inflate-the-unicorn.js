9cbab783e8528b97d5ebee3b8d6f11b410307ff6
Nixon Cam <nixon.cam997@gmail.com>
2025-07-17 10:35:15 +1200

    unicorn shoots confetti

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -2,4 +2,31 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
+let unicornStages = [0, 0, 0]
+
+const unicorns = document.querySelectorAll('.inflate-an-image')
+
+unicorns.forEach((unicorn, index) => {
+  unicorn.onclick = () => {
+    inflateUnicorn(index)
+  }
+})
+
+function inflateUnicorn(index) {
+  if (unicornStages[index] < 3) {
+    unicornStages[index]++
+    unicorns[index].src = `./images/unicorn-${unicornStages[index]}.png`
+
+    if (unicornStages[index] === 3) {
+      confetti() // 🎉 Unicorn fully inflated!
+    }
+  }
+}
+
+
+
+
+
+
+
 console.log("Inflate The Unicorn!")
